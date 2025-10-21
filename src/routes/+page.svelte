580a2420e88af0d536a461a7bdcfe7242fc4e794
Sean Lynch <techniq35@gmail.com>
2025-10-20 21:22:56 -0400

    Add name and abbreciation labels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Chart, GeoPath, Layer } from 'layerchart';
+	import { Chart, GeoPath, Layer, Text } from 'layerchart';
 	import { geoAlbersUsa } from 'd3-geo';
 	import { feature } from 'topojson-client';
 	import { cls } from '@layerstack/tailwind';
@@ -8,58 +8,111 @@
 	const states = feature(data.geojson, data.geojson.objects.states);
 
 	const selectedState = $state({
-		Alabama: true,
+		Alabama: false,
 		Alaska: false,
-		Arizona: true,
-		Arkansas: true,
-		California: true,
-		Colorado: true,
-		Connecticut: true,
-		Delaware: true,
-		Florida: true,
-		Georgia: true,
+		Arizona: false,
+		Arkansas: false,
+		California: false,
+		Colorado: false,
+		Connecticut: false,
+		Delaware: false,
+		Florida: false,
+		Georgia: false,
 		Hawaii: false,
-		Idaho: true,
-		Illinois: true,
-		Indiana: true,
-		Iowa: true,
-		Kansas: true,
-		Kentucky: true,
-		Louisiana: true,
-		Maine: true,
-		Maryland: true,
-		Massachusetts: true,
-		Michigan: true,
-		Minnesota: true,
-		Mississippi: true,
-		Missouri: true,
-		Montana: true,
-		Nebraska: true,
-		Nevada: true,
-		'New Hampshire': true,
-		'New Jersey': true,
+		Idaho: false,
+		Illinois: false,
+		Indiana: false,
+		Iowa: false,
+		Kansas: false,
+		Kentucky: false,
+		Louisiana: false,
+		Maine: false,
+		Maryland: false,
+		Massachusetts: false,
+		Michigan: false,
+		Minnesota: false,
+		Mississippi: false,
+		Missouri: false,
+		Montana: false,
+		Nebraska: false,
+		Nevada: false,
+		'New Hampshire': false,
+		'New Jersey': false,
 		'New Mexico': false,
-		'New York': true,
-		'North Carolina': true,
-		'North Dakota': true,
-		Ohio: true,
-		Oklahoma: true,
+		'New York': false,
+		'North Carolina': false,
+		'North Dakota': false,
+		Ohio: false,
+		Oklahoma: false,
 		Oregon: false,
-		Pennsylvania: true,
-		'Rhode Island': true,
-		'South Carolina': true,
-		'South Dakota': true,
-		Tennessee: true,
-		Texas: true,
+		Pennsylvania: false,
+		'Rhode Island': false,
+		'South Carolina': false,
+		'South Dakota': false,
+		Tennessee: false,
+		Texas: false,
 		Utah: false,
 		Vermont: false,
-		Virginia: true,
-		Washington: true,
-		'West Virginia': true,
-		Wisconsin: true,
-		Wyoming: true
+		Virginia: false,
+		Washington: false,
+		'West Virginia': false,
+		Wisconsin: false,
+		Wyoming: false
 	});
 	// Ontario
+
+	const abbreviations = {
+		Alabama: 'AL',
+		Alaska: 'AK',
+		Arizona: 'AZ',
+		Arkansas: 'AR',
+		California: 'CA',
+		Colorado: 'CO',
+		Connecticut: 'CT',
+		Delaware: 'DE',
+		Florida: 'FL',
+		Georgia: 'GA',
+		Hawaii: 'HI',
+		Idaho: 'ID',
+		Illinois: 'IL',
+		Indiana: 'IN',
+		Iowa: 'IA',
+		Kansas: 'KS',
+		Kentucky: 'KY',
+		Louisiana: 'LA',
+		Maine: 'ME',
+		Maryland: 'MD',
+		Massachusetts: 'MA',
+		Michigan: 'MI',
+		Minnesota: 'MN',
+		Mississippi: 'MS',
+		Missouri: 'MO',
+		Montana: 'MT',
+		Nebraska: 'NE',
+		Nevada: 'NV',
+		'New Hampshire': 'NH',
+		'New Jersey': 'NJ',
+		'New Mexico': 'NM',
+		'New York': 'NY',
+		'North Carolina': 'NC',
+		'North Dakota': 'ND',
+		Ohio: 'OH',
+		Oklahoma: 'OK',
+		Oregon: 'OR',
+		Pennsylvania: 'PA',
+		'Rhode Island': 'RI',
+		'South Carolina': 'SC',
+		'South Dakota': 'SD',
+		Tennessee: 'TN',
+		Texas: 'TX',
+		Utah: 'UT',
+		Vermont: 'VT',
+		Virginia: 'VA',
+		Washington: 'WA',
+		'West Virginia': 'WV',
+		Wisconsin: 'WI',
+		Wyoming: 'WY'
+	};
 </script>
 
 <div class="h-[800px] p-4">
@@ -87,6 +140,34 @@
 						selectedState[state.properties.name] = !selectedState[state.properties.name];
 					}}
 				/>
+			{/each}
+
+			{#each states.features as state}
+				{#if selectedState[state.properties.name]}
+					<GeoPath geojson={state}>
+						{#snippet children({ geoPath })}
+							{@const [x, y] = geoPath?.centroid(state) ?? [0, 0]}
+							<Text
+								{x}
+								{y}
+								dy={-4}
+								value={abbreviations[state.properties.name]}
+								textAnchor="middle"
+								verticalAnchor="middle"
+								class="stroke-surface-100 stroke-2 text-lg"
+							/>
+							<Text
+								{x}
+								{y}
+								dy={6}
+								value={state.properties.name}
+								textAnchor="middle"
+								verticalAnchor="middle"
+								class="fill-surface-content stroke-surface-100 stroke-2 text-[8px]"
+							/>
+						{/snippet}
+					</GeoPath>
+				{/if}
 			{/each}
 		</Layer>
 	</Chart>
